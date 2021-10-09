4481a5cc24ce9f1af158c6a834493d8f74d4462c
Luan Nascimento <luanoliveira1107@gmail.com>
2021-10-09 17:38:07 -0300

    testePeopleStarWars people root.

---
+++ testeComRestAssured/src/test/java/people.java
@@ -0,0 +1,39 @@
+import io.restassured.RestAssured;
+
+import io.restassured.http.ContentType;
+import io.restassured.parsing.Parser;
+import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
+import org.apache.http.HttpStatus;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.get;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class people {
+
+    @BeforeAll
+    public static void setup(){
+        RestAssured.baseURI = "https://swapi.dev/api";
+    }
+
+    @Test
+    public void testPeopleStarWars(){
+        RestAssured.defaultParser = Parser.JSON;
+     Response response =  given()
+             .header("Content-Type", ContentType.JSON, "Accept", ContentType.JSON)
+             .get("/people/1/")
+             .then().contentType(ContentType.JSON)
+             .extract()
+             .response();
+
+        String nameRoot = response.jsonPath().getString("name");
+       assertEquals("Luke Skywalker", nameRoot);
+    }
+
+
+}
